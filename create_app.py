fda6860c9a8dc5317269a1b9d11768704fa5eff9
gpq291 <1335871880@qq.com>
2025-03-23 21:48:21 +0800

    connector

--- create_app.py
+++ create_app.py
@@ -1,17 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-
+import mysql.connector
 
 app = Flask(__name__)
-
-HOSTNAME = "121.41.170.185"
-PORT = 3306
-USERNAME = "root"
-PASSWORD = "!200408Wpprpi!"
-DATABASE = "travel4"
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8"
-
-
-db = SQLAlchemy(app)
+connection = mysql.connector.connect(
+    username = 'root',
+    password = '20040229w',
+    host = 'localhost',
+    database = 'new_schema'
+)

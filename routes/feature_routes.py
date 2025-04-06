c51afdc5f85b2e26e6be575a13a059dc6b262c76
gpq291 <1335871880@qq.com>
2025-04-06 16:57:22 +0800

    first connect

--- routes/feature_routes.py
+++ routes/feature_routes.py
@@ -1,100 +1,101 @@
 from flask import Blueprint, request, jsonify
-from sqlalchemy.sql import func
-from model.db_model import Comments, pois
-from parser.models_parser import poi_list_view_item, detail_item
+
 from parser.msg_parser import success, args_missing, data_not_exist
-from create_app import db
 
 feature_bp = Blueprint('feature', __name__)
 
 @feature_bp.route('/homepage', methods=['GET'])
 def get_items():
-    type = request.args.get('type')
-    i = request.args.get('i')
+    pass
+    # type = request.args.get('type')
+    # i = request.args.get('i')
 
-    if not (type and i):
-        return jsonify(args_missing())
+    # if not (type and i):
+    #     return jsonify(args_missing())
     
-    i=int(i)
+    # i=int(i)
     
-    a = 20*i;
-    b = a-20
+    # a = 20*i;
+    # b = a-20
 
-    POIs = pois.query.filter_by(ptype=type).slice(b, a).all()
-    pois_list = [poi_list_view_item(poi) for poi in POIs]
+    # POIs = pois.query.filter_by(ptype=type).slice(b, a).all()
+    # pois_list = [poi_list_view_item(poi) for poi in POIs]
 
-    return jsonify(success(pois_list))
+    # return jsonify(success(pois_list))
 
 @feature_bp.route('/mapview', methods=['GET'])
 def get_item():
-    id = request.args.get('id')
-    POI = pois.query.filter_by(pid=id).first()
-    return jsonify(success(poi_list_view_item(POI)))
+    pass
+    # id = request.args.get('id')
+    # POI = pois.query.filter_by(pid=id).first()
+    # return jsonify(success(poi_list_view_item(POI)))
 
 @feature_bp.route('/detail', methods=['GET'])
 def fuzzy_search():
-    id = request.args.get('id')
+    pass
+    # id = request.args.get('id')
 
-    if not id:
-        return jsonify(args_missing)
+    # if not id:
+    #     return jsonify(args_missing)
     
-    id = int(id)
-    poi = pois.query.filter_by(pid=id).first()
+    # id = int(id)
+    # poi = pois.query.filter_by(pid=id).first()
 
-    if not poi:
-        return jsonify(data_not_exist())
+    # if not poi:
+    #     return jsonify(data_not_exist())
     
-    result = detail_item(poi)
+    # result = detail_item(poi)
 
-    query = db.session.query(
-        func.ST_X(pois.plocation).label('x'),
-        func.ST_Y(pois.plocation).label('y'),
-    ).filter_by(pid=id).first()
-    coordination = {key: value for key, value in query._asdict().items()}
+    # query = db.session.query(
+    #     func.ST_X(pois.plocation).label('x'),
+    #     func.ST_Y(pois.plocation).label('y'),
+    # ).filter_by(pid=id).first()
+    # coordination = {key: value for key, value in query._asdict().items()}
 
-    result['plocation'] = coordination
+    # result['plocation'] = coordination
 
-    return jsonify(success(result))
+    # return jsonify(success(result))
 
 
 @feature_bp.route('/comments', methods=['GET'])
 def get_poi_comment():
-
-    id = request.args.get('id')
-    if not id:
-        return jsonify(args_missing)
+    pass
+    # id = request.args.get('id')
+    # if not id:
+    #     return jsonify(args_missing)
     
-    id = int(id)
-    comments_list = Comments.query.filter_by(cpid=id).all()
-    print(type(comments_list[0]))
+    # id = int(id)
+    # comments_list = Comments.query.filter_by(cpid=id).all()
+    # print(type(comments_list[0]))
 
-    result = [detail_item(com) for com in comments_list]
-    return jsonify(success(result))
+    # result = [detail_item(com) for com in comments_list]
+    # return jsonify(success(result))
 
 
 
 @feature_bp.route('/search', methods=['GET'])
 def get_item_Detail():
-    search_keyWord = request.args.get('name')
-    type = request.args.get('type')
+    pass
+    # search_keyWord = request.args.get('name')
+    # type = request.args.get('type')
 
-    if not search_keyWord:
-        return jsonify(args_missing)
+    # if not search_keyWord:
+    #     return jsonify(args_missing)
 
-    query = db.session.query(
-        pois.pname, pois.pintroduce_short, pois.paddress,
-        func.ST_X(pois.plocation).label('x'),
-        func.ST_Y(pois.plocation).label('y'),
-    ).filter(
-        pois.ptype == type,
-        pois.pname.like(f'%{search_keyWord}%')
-    ).limit(5).all()
+    # query = db.session.query(
+    #     pois.pname, pois.pintroduce_short, pois.paddress,
+    #     func.ST_X(pois.plocation).label('x'),
+    #     func.ST_Y(pois.plocation).label('y'),
+    # ).filter(
+    #     pois.ptype == type,
+    #     pois.pname.like(f'%{search_keyWord}%')
+    # ).limit(5).all()
     
-    result = [];
+    # result = [];
 
-    for item in query:
-        Dict = {key: value for key, value in item._asdict().items()}
-        result.append(Dict)
+    # for item in query:
+    #     Dict = {key: value for key, value in item._asdict().items()}
+    #     result.append(Dict)
 
 
-    return jsonify(success(result))
+    # return jsonify(success(result))
--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,12 +1,11 @@
 import json
 import os
 from flask import Blueprint, jsonify, request, send_from_directory
-from sqlalchemy.orm.exc import NoResultFound
-from model.db_model import Event, Record, Users
-from create_app import db
-from parser.models_parser import to_dict
-from parser.msg_parser import args_missing, data_not_exist, success
-
+#from sqlalchemy.orm.exc import NoResultFound
+#from model.db_model import Event, Record, Users
+from parser.msg_parser import success, args_missing, data_not_exist
+from create_app import connection
+from mysql.connector import Error
 
 user_bp = Blueprint('user', __name__, url_prefix='/user')
 
@@ -14,122 +13,148 @@ user_bp = Blueprint('user', __name__, url_prefix='/user')
 def home():
     return "???"
 
+    
+    
 @user_bp.route("/login", methods=['GET'])
 def login():
-    name = request.args.get('name')
+    username = request.args.get('name')
     password = request.args.get('password')
+    if not username or not password:
+        return jsonify({"success": False, "message": "Username and password are required"}), 400
+    if not connection:
+        return jsonify({"success": False, "message": "Database connection error"}), 502
+    try:
+        cursor = connection.cursor(dictionary=True) 
+        query = "SELECT * FROM users WHERE username = %s AND password = %s"
+        cursor.execute(query, (username, password))
+        user = cursor.fetchone()
+        if user:
+            return jsonify(success(user))
+        else:
+            return jsonify({"success": False, "message": "Invalid username or password"}), 401
 
-    query = db.session.query(
-        Users.uid, Users.unickname, Users.upic
-    ).filter(
-        Users.uname == name,
-        Users.upassword == password
-    ).limit(1)
+    except Error as e:
+        return jsonify({"success": False, "message": f"Database error: {str(e)}"}), 501
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            #connection.close()
+    return f"${name}, ${password}"
+
+    # query = db.session.query(
+    #     Users.uid, Users.unickname, Users.upic
+    # ).filter(
+    #     Users.uname == name,
+    #     Users.upassword == password
+    # ).limit(1)
     # <class 'sqlalchemy.engine.row.Row'>
 
-    try:
-        result = query.one()
-        Dict = {key: value for key, value in result._asdict().items()}
-        return jsonify(success(Dict))
-    except NoResultFound:
-        return jsonify(data_not_exist())
+    # try:
+    #     result = query.one()
+    #     Dict = {key: value for key, value in result._asdict().items()}
+    #     return jsonify(success(Dict))
+    # except NoResultFound:
+    #     return jsonify(data_not_exist())
 
     
 
 @user_bp.route("/signup")
 def add_user():
-    user = Users(uname="a", upassword="1234556")
-    db.session.add(user)
-    db.session.commit()
-    return "创建成功"
+    pass
+    # user = Users(uname="a", upassword="1234556")
+    # db.session.add(user)
+    # db.session.commit()
+    # return "创建成功"
 
 
 @user_bp.route("/create_record", methods=['POST'])
 def create_record():
+    pass
+    # data = request.get_json()
+    # print(data)
+    # id = data['id']
+    # point = data['start']
+    # name = data['name']
+    # print(name)
 
-    data = request.get_json()
-    print(data)
-    id = data['id']
-    point = data['start']
-    name = data['name']
-    print(name)
+    # record = Record(uid = id, point = [point], name=name)
+    # print(record.name)
 
-    record = Record(uid = id, point = [point], name=name)
-    print(record.name)
+    # db.session.add(record)
+    # db.session.commit()
 
-    db.session.add(record)
-    db.session.commit()
-
-    return jsonify(success({"id": record.id}))
+    # return jsonify(success({"id": record.id}))
 
 
 
 @user_bp.route("/records", methods=['GET'])
 def get_records():
-    id = request.args.get('id')
+    pass
+    # id = request.args.get('id')
 
-    record_list =  db.session.query(
-        Record.id, Record.name,
-    ).filter_by(uid=id).all()
-    #print(type(record_list[0]))
+    # record_list =  db.session.query(
+    #     Record.id, Record.name,
+    # ).filter_by(uid=id).all()
+    # #print(type(record_list[0]))
 
-    result_list = []
-    for record in record_list:
-        Dict = {key: value for key, value in record._asdict().items()}
-        print(dict)
+    # result_list = []
+    # for record in record_list:
+    #     Dict = {key: value for key, value in record._asdict().items()}
+    #     print(dict)
 
-        result_list.append(Dict)
+    #     result_list.append(Dict)
 
-    print(result_list)
-    return jsonify(success(result_list))
+    # print(result_list)
+    # return jsonify(success(result_list))
 
 @user_bp.route("/recordDetail", methods=['GET'])
 def get_record_detail():
-    id = request.args.get('id')
+    pass
+    # id = request.args.get('id')
 
-    record = Record.query.filter_by(id=id).first()
+    # record = Record.query.filter_by(id=id).first()
 
 
-    query = Event.query.filter_by(rid=id).all()
+    # query = Event.query.filter_by(rid=id).all()
 
-    event_list = []
-    for event in query:
-        Dict = to_dict(event)
-        event_list.append(Dict)
-    #print(type(result), type(event_list))
+    # event_list = []
+    # for event in query:
+    #     Dict = to_dict(event)
+    #     event_list.append(Dict)
+    # #print(type(result), type(event_list))
 
-    result = {"points": json.loads(record.point),
-              "events": event_list},
+    # result = {"points": json.loads(record.point),
+    #           "events": event_list},
 
-    return jsonify(success(result))
+    # return jsonify(success(result))
 
 
 
 @user_bp.route("/push_point", methods=['POST'])
 def push_point():
+    pass
+    # data = request.get_json()
+    # id = data['id']
+    # print(id)
+    # newPoint = data['point']
 
-    data = request.get_json()
-    id = data['id']
-    print(id)
-    newPoint = data['point']
-
-    record = Record.query.get(id)
-    if not isinstance(record.point, list):
-        if isinstance(record.point, str):
-            record.point = json.loads(record.point)
-        else:
-            record.point = [] 
+    # record = Record.query.get(id)
+    # if not isinstance(record.point, list):
+    #     if isinstance(record.point, str):
+    #         record.point = json.loads(record.point)
+    #     else:
+    #         record.point = [] 
     
-    #print(record.point)
-    record.point.append(newPoint)
-    record.point = json.dumps(record.point)
-    #print(record.point)
-    try:
-        db.session.commit()
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({"error": str(e)}), 500
-    return jsonify(success({}))
+    # #print(record.point)
+    # record.point.append(newPoint)
+    # record.point = json.dumps(record.point)
+    # #print(record.point)
+    # try:
+    #     db.session.commit()
+    # except Exception as e:
+    #     db.session.rollback()
+    #     return jsonify({"error": str(e)}), 500
+    # return jsonify(success({}))
 
 
 @user_bp.route('/insertEvent', methods=['POST'])
